21ad9179588de5f3026ffe431d698c44894f2da4
i-groot <472164954@qq.com>
2020-08-04 22:31:42 +0800

    first add Main.java

--- src/main/java/com/groot/test/Main.java
+++ src/main/java/com/groot/test/Main.java
@@ -4,5 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World.");
+        int ans = getAns();
+        System.out.println(ans);
+    }
+
+    private static int getAns() {
+        return 10 + 15;
     }
 }
