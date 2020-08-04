d0527838a358826a554b6cbdbe32f15fad7e62c4
i-groot <472164954@qq.com>
2020-08-04 22:31:12 +0800

    modify sec

--- src/main/java/com/groot/test/Main.java
+++ src/main/java/com/groot/test/Main.java
@@ -2,7 +2,11 @@ package com.groot.test;
 
 public class Main {
 
+    private static String haha;
+
     public static void main(String[] args) {
+        haha = "haha";
         System.out.println("Hello World.");
+        System.out.println("This will be merged.");
     }
 }
