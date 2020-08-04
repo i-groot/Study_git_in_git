d36dab04c8539af0a380bf17362e787887412230
i-groot <472164954@qq.com>
2020-08-04 22:45:06 +0800

    modify sec

--- src/main/java/com/groot/test/Main.java
+++ src/main/java/com/groot/test/Main.java
@@ -1,5 +1,9 @@
 package com.groot.test;
 
+import com.sun.xml.internal.ws.util.StringUtils;
+
+import java.util.Objects;
+
 public class Main {
 
     private static String haha;
@@ -8,5 +12,16 @@ public class Main {
         haha = "haha";
         System.out.println("Hello World.");
         System.out.println("This will be merged.");
+        System.out.println(countChar("Hello World.", 'l'));
+    }
+    
+    private static int countChar(String str, char ch) {
+        if (Objects.isNull(str)) return 0;
+        byte[] bytes = str.getBytes();
+        int count = 0;
+        for (byte b : bytes) {
+            if (b == ch) count++;
+        }
+        return count;
     }
 }
